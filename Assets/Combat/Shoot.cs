3d31479c65f3b363106dd741af741eb639abfc5a
Matt Perry <mpcomplete@gmail.com>
2023-07-04 16:12:58 -0400

    Projectiles work

--- Assets/Combat/Shoot.cs
+++ Assets/Combat/Shoot.cs
@@ -1,28 +1,40 @@
-using Archero;
+using System;
 using UnityEngine;
 
 // Identifier for which team a character is on - e.g., player or mob.
 public class Shoot : MonoBehaviour {
-  public Projectile Projectile;
-  public int Count;  // Changes in runtime
+  [Serializable]
+  public struct Variant {
+    public GameObject Projectile;
+    public float AttacksPerSecond;
+  }
+  public Variant[] ProjectileVariants;
+  public int CurrentVariant = 0;
+  //public InputAction Axis;
 
-  //public struct GunType {
-  //  public int Count;
-  //  public float HorizontalSpacing;
-  //  public float[] Angles;
-  //}
+  static GameObject _Parent;
+  static GameObject Parent => _Parent = _Parent ? _Parent : new GameObject("Projectiles");
 
-  //void FireArrowVolley(Vector3 direction, int count) {
-  //  var spreadDirection = Vector3.Cross(direction, Vector3.up);
-  //  var spreadTotal = count == 0 ? 0 : (count-1) * SpreadDistance;
-  //  var spreadHalf = spreadTotal / 2;
-  //  for (var i = 0; i < count; i++) {
-  //    var origin = transform.position + ShootOffset + (i * SpreadDistance - spreadHalf) * spreadDirection;
-  //    Projectile.Fire(ProjectilePrefab, origin, Quaternion.LookRotation(direction), Attributes, HitConfig);
-  //  }
-  //}
+  public void Spawn(Variant variant, Vector3 dir) {
+    var p = Instantiate(variant.Projectile, transform.position, Quaternion.LookRotation(dir));
+    p.transform.SetParent(Parent.transform, true);
+  }
 
-  //public void Shoot(Vector3 direction) {
+  int CooldownTicksRemaining = 0;
+  public void FixedUpdate() {
+    if (CooldownTicksRemaining > 0) {
+      CooldownTicksRemaining--;
+    } else {
+      var variant = ProjectileVariants[CurrentVariant];
+      var axis = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+      if (axis.sqrMagnitude > 0.1f.Sqr()) {
+        CooldownTicksRemaining = Timeval.FromSeconds(1f / variant.AttacksPerSecond).Ticks;
+        Spawn(variant, axis.XZ());
+      }
+    }
+  }
 
-  //}
+  private void Awake() {
+    Time.fixedDeltaTime = 1f / Timeval.FixedUpdatePerSecond;
+  }
 }
