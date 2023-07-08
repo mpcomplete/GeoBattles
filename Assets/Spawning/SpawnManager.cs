c461ed5b5645c3b3ab9fb60602c413434bd91dba
Steve Kane <kanesteven@gmail.com>
2023-07-08 15:16:17 -0500

    spawnmanager instance is static

--- Assets/Spawning/SpawnManager.cs
+++ Assets/Spawning/SpawnManager.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour {
-  public SpawnManager Instance;
+  public static SpawnManager Instance;
+
   SpawnEvent[] SpawnEvents;
   SpawnEvent CurrentSpawnEvent;
 
