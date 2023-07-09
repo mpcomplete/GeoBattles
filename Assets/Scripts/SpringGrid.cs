118701b465cc3de1dfce966284fda2c83a173089
Steve Kane <kanesteven@gmail.com>
2023-07-08 21:18:24 -0500

    converted springgrid to use unity mathematics. enabled full unsafe burst. Can multiply the resolution of springgrid by 10

--- Assets/Scripts/SpringGrid.cs
+++ Assets/Scripts/SpringGrid.cs
@@ -2,6 +2,7 @@ using Unity.Burst;
 using Unity.Collections;
 using Unity.Jobs;
 using UnityEngine;
+using Unity.Mathematics;
 
 public class SpringGrid : MonoBehaviour {
   public MeshFilter Mesh;
@@ -10,11 +11,18 @@ public class SpringGrid : MonoBehaviour {
   public float SpringDamping = 10f;
   public float Mass = 1;
 
+  /*
+    Steve - Write from native array to this before assigning to mesh. So stupid that
+    this is required but there is no implicit conversion from NativeArray<float3> or
+    float3[] to Vector3[]. Thus, we copy the job data from the JobBuffer to this array
+    then into the mesh... god have mercy on us.
+  */
+  Vector3[] Vertices;
   int XCells, ZCells;
   int XVertices, ZVertices;
   struct Buffer {
-    public NativeArray<Vector3> Vertices;
-    public NativeArray<Vector3> Velocities;
+    public NativeArray<float3> Vertices;
+    public NativeArray<float3> Velocities;
   }
   Buffer[] JobBuffer = new Buffer[2];
   int BufferIdx = 0;
@@ -33,10 +41,10 @@ public class SpringGrid : MonoBehaviour {
 
   [BurstCompile]
   struct SpringSolverJob : IJobParallelFor {
-    [ReadOnly] public NativeArray<Vector3> LastVertices;
-    [ReadOnly] public NativeArray<Vector3> LastVelocities;
-    public NativeArray<Vector3> Vertices;
-    public NativeArray<Vector3> Velocities;
+    [ReadOnly] public NativeArray<float3> LastVertices;
+    [ReadOnly] public NativeArray<float3> LastVelocities;
+    public NativeArray<float3> Vertices;
+    public NativeArray<float3> Velocities;
     [ReadOnly] public NativeArray<PForce> Forces;
     public int NumForces;
     const float SpringK = 20f;
@@ -44,24 +52,26 @@ public class SpringGrid : MonoBehaviour {
     const float Mass = 1f;
     public float DeltaTime;
 
-    public Vector2 GridSpacing;
+    public float BoundsXMin;
+    public float BoundsZMin;
+    public float2 GridSpacing;
     public int XVertices;
     public int ZVertices;
     int VertexIndex(int x, int z) => z*XVertices + x;
     (int, int) GridIndex(int vi) => (vi % XVertices, vi / XVertices);
-    Vector3 GridToWorld(int x, int z) => new(Bounds.Instance.XMin + x*GridSpacing.x, 0f, Bounds.Instance.ZMin + z*GridSpacing.y);
+    float3 GridToWorld(int x, int z) => new float3(BoundsXMin + x*GridSpacing.x, 0f, BoundsZMin + z*GridSpacing.y);
 
     public void Execute(int i) {
       (var x, var z) = GridIndex(i);
       Vertices[i] = LastVertices[i];
       Velocities[i] = LastVelocities[i];
-      IterateSprings(x, z, i);  // must call after
+      IterateSprings(x, z, i);
       ApplyProjectileForces(x, z, i);
     }
 
-    Vector3 NeighborF(Vector3 p, int nx, int nz) {
+    float3 NeighborF(float3 p, int nx, int nz) {
       if (nx < 0 || nx >= XVertices || nz < 0 || nz >= ZVertices)
-        return -SpringK * (p - GridToWorld(nx, nz));  // pretend there's a fixed neighbor for the edge vertices
+        return -SpringK * (p - GridToWorld(nx, nz));
       var nvi = VertexIndex(nx, nz);
       var np = LastVertices[nvi];
       return -SpringK * (p - np);
@@ -70,14 +80,12 @@ public class SpringGrid : MonoBehaviour {
     void IterateSprings(int x, int z, int vi) {
       var p = Vertices[vi];
       var v = Velocities[vi];
-      //var k = SpringKLocal[vi];
-      //SpringKLocal[vi] = Mathf.Lerp(SpringKLocal[vi], SpringK, .5f);
       var anchor = GridToWorld(x, z);
       var springF = -SpringK*(p - anchor);
       var dampingF = SpringDamping * v;
       var neighborF = NeighborF(p, x-1, z) + NeighborF(p, x+1, z) + NeighborF(p, x, z-1) + NeighborF(p, x, z+1);
       var force = neighborF + springF - dampingF;
-      v += DeltaTime * force / Mass; // mass=1
+      v += DeltaTime * force / Mass;
       p += DeltaTime * v;
 
       Vertices[vi] = p;
@@ -94,17 +102,18 @@ public class SpringGrid : MonoBehaviour {
       }
     }
 
-    void ApplyVelocity(int x, int z, int vi, Vector3 pos, Vector3 vel, float mass, float maxDistance) {
-      var vnorm = vel.normalized;
-      var tangent = Vector3.Cross(vnorm, Vector3.up);
+    void ApplyVelocity(int x, int z, int vi, float3 pos, float3 vel, float mass, float maxDistance) {
+      var vnorm = math.normalize(vel);
+      var tangent = math.cross(vnorm, new float3(0,1,0));
       var p = Vertices[vi];
       var towardsVert = p - pos;
-      if (towardsVert.sqrMagnitude > maxDistance.Sqr()) return;
+      if (math.lengthsq(towardsVert) > maxDistance*maxDistance) return;
 
-      var rightSide = Vector3.Dot(towardsVert, tangent) > 0f;
-      if (!rightSide) tangent = -tangent;
-      var baseF = Vector3.Lerp(tangent, vnorm, ProjectileForwardBias).normalized;
-      Velocities[vi] += ProjectileFactor * mass / towardsVert.magnitude * baseF;
+      var rightSide = math.dot(towardsVert, tangent) > 0f;
+      tangent = rightSide ? tangent : -tangent;
+      var baseF = math.normalizesafe(math.lerp(tangent, vnorm, ProjectileForwardBias));
+      float3 velocity = ProjectileFactor * mass / math.length(towardsVert) * baseF;
+      Velocities[vi] += velocity;
       //SpringKLocal[vi] += mass / towardsVert.magnitude;
     }
   }
@@ -120,10 +129,10 @@ public class SpringGrid : MonoBehaviour {
     ZVertices = ZCells+1;
 
     int nverts = XVertices * ZVertices;
-    JobBuffer[0].Vertices = new NativeArray<Vector3>(nverts, Allocator.Persistent);
-    JobBuffer[0].Velocities = new NativeArray<Vector3>(nverts, Allocator.Persistent);
-    JobBuffer[1].Vertices = new NativeArray<Vector3>(nverts, Allocator.Persistent);
-    JobBuffer[1].Velocities = new NativeArray<Vector3>(nverts, Allocator.Persistent);
+    JobBuffer[0].Vertices = new NativeArray<float3>(nverts, Allocator.Persistent);
+    JobBuffer[0].Velocities = new NativeArray<float3>(nverts, Allocator.Persistent);
+    JobBuffer[1].Vertices = new NativeArray<float3>(nverts, Allocator.Persistent);
+    JobBuffer[1].Velocities = new NativeArray<float3>(nverts, Allocator.Persistent);
     Forces = new NativeArray<PForce>(500, Allocator.Persistent);
     //SpringKLocal = new float[Vertices.Length];
     for (int z = 0; z < ZVertices; z++) {
@@ -164,13 +173,22 @@ public class SpringGrid : MonoBehaviour {
     }
 
     var m = new Mesh();
-    m.vertices = JobBuffer[0].Vertices.ToArray();
+    var vertexCount = JobBuffer[0].Vertices.Length;
+    // TODO: SO stupid... implicit conversion from
+    Vertices = new Vector3[vertexCount];
+    for (var i = 0; i < vertexCount; i++)
+      Vertices[i] = JobBuffer[0].Vertices[i];
+    m.vertices = Vertices;
     m.triangles = triangles;
     m.uv = uvs;
+    m.RecalculateTangents();
     m.RecalculateNormals();
+    m.UploadMeshData(false);
     Mesh.mesh = m;
 
     Job = new SpringSolverJob() {
+      BoundsXMin = Bounds.Instance.XMin,
+      BoundsZMin = Bounds.Instance.ZMin,
       LastVertices = JobBuffer[0].Vertices,
       LastVelocities = JobBuffer[0].Velocities,
       Vertices = JobBuffer[1].Vertices,
@@ -190,7 +208,14 @@ public class SpringGrid : MonoBehaviour {
     JobHandle.Complete();
     if (!JobHandle.IsCompleted)
       Debug.Log($"Job: {JobHandle.IsCompleted}");
-    Mesh.mesh.vertices = Job.Vertices.ToArray();
+    var vertexCount = JobBuffer[0].Vertices.Length;
+    // TODO: SO stupid... implicit conversion from
+    for (var i = 0; i < vertexCount; i++)
+      Vertices[i] = JobBuffer[0].Vertices[i];
+    Mesh.mesh.vertices = Vertices;
+    Mesh.mesh.RecalculateNormals();
+    Mesh.mesh.UploadMeshData(false);
+
     //var tmp = Job.Velocities.ToArray();
     //var vi = 0;
     //foreach (var v in tmp) {
@@ -204,6 +229,8 @@ public class SpringGrid : MonoBehaviour {
     BufferIdx = writeIdx;
     int numForces = ApplyProjectileForces();
     Job = new SpringSolverJob() {
+      BoundsXMin = Bounds.Instance.XMin,
+      BoundsZMin = Bounds.Instance.ZMin,
       LastVertices = JobBuffer[readIdx].Vertices,
       LastVelocities = JobBuffer[readIdx].Velocities,
       Vertices = JobBuffer[writeIdx].Vertices,
