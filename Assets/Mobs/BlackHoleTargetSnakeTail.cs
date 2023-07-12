777ce2649fe17a694d6aa1a30f32444a05795202
Matt Perry <mpcomplete@gmail.com>
2023-07-11 23:05:41 -0400

    Ensure snake cleanup on sucking blackhole death

--- Assets/Mobs/BlackHoleTargetSnakeTail.cs
+++ Assets/Mobs/BlackHoleTargetSnakeTail.cs
@@ -1,17 +1,28 @@
+using UnityEngine;
+
 public class BlackHoleTargetSnakeTail : BlackHoleTarget {
   public MoveSnakeTail Tail;
   public int Index;  // Initialized by MoveSnakeTail.
+  public float VelocityDamping = .9f;
 
   public override void OnEaten(BlackHole hole) {
     if (Tail) {
-      Tail.OnTailEaten(Index);
+      Tail.OnTailEaten(hole, Index);
       gameObject.SetActive(false);
     } else {
       Destroy(gameObject);
     }
   }
 
-  public void OnOtherEaten(int index) {
+  Vector3 Velocity;
+  public override void Suck(Vector3 accel) {
+    var a = GravityVulnerability * accel;
+    Velocity += Time.fixedDeltaTime * a;
+  }
+
+  private void FixedUpdate() {
+    transform.position += Time.fixedDeltaTime * Velocity;
+    Velocity *= VelocityDamping;
   }
 
   void Awake() {
